8fbac48375fedced698e36ffe7bb6348c5a459dc
GCaptainNemo <1136003685@qq.com>
2021-04-16 16:02:34 +0800

    modify main.py

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ heatequation = Heat(net, te, xe, ye)
 
 train = Train(net, heatequation, BATCH_SIZE=2**8)
 
-train.train(epoch=105, lr=0.0001)
+train.train(epoch=10**5, lr=0.0001)
 
 torch.save(net, '../model/net_model.pkl')
 
@@ -43,46 +43,45 @@ data = np.empty((3, 1))
 
 k = 0
 for _t in t_range:
-    if _t % 100 == 0:
-        TrueZ = []
-        Z = []
-        data[0] = _t
-        for _x in x_range:
-            data[1] = _x
-            for _y in y_range:
-                data[2] = _y
-                indata = torch.Tensor(data.reshape(1, -1))
-                Zdata = net(indata).detach().cpu().numpy()
-                Z.append(Zdata)
-                TrueZ.append(np.sin(np.pi*_t)*np.sin(np.pi*_x)*np.sin(np.pi*_y))
+    TrueZ = []
+    Z = []
+    data[0] = _t
+    for _x in x_range:
+        data[1] = _x
+        for _y in y_range:
+            data[2] = _y
+            indata = torch.Tensor(data.reshape(1, -1))
+            Zdata = net(indata).detach().cpu().numpy()
+            Z.append(Zdata)
+            TrueZ.append(np.sin(np.pi*_t)*np.sin(np.pi*_x)*np.sin(np.pi*_y))
 
 
-        _X, _Y = np.meshgrid(x_range, y_range, indexing='ij')
+    _X, _Y = np.meshgrid(x_range, y_range, indexing='ij')
 
-        Z_surface = np.reshape(Z, (x_range.shape[0], y_range.shape[0]))
-        True_Z_surface = np.reshape(TrueZ, (x_range.shape[0], y_range.shape[0]))
+    Z_surface = np.reshape(Z, (x_range.shape[0], y_range.shape[0]))
+    True_Z_surface = np.reshape(TrueZ, (x_range.shape[0], y_range.shape[0]))
 
-        # plot the approximated values
-        fig = plt.figure()
-        ax = fig.gca(projection='3d')
-        ax.set_zlim([-1, 1])
-        ax.plot_surface(_X, _Y, Z_surface, cmap=cm.RdYlBu_r, edgecolor='blue', linewidth=0.0003, antialiased=True)
-        ax.set_xlabel('x')
-        ax.set_ylabel('y')
-        ax.set_zlabel('u')
-        path = "../pictures/approxmate_solution/%i.png" % k
-        plt.savefig(path)
-        plt.close(fig)
-        # plot the exact solution
-        fig = plt.figure()
-        ax = fig.gca(projection='3d')
-        ax.set_zlim([-1, 1])
-        ax.plot_surface(_X, _Y, True_Z_surface, cmap=cm.RdYlBu_r, edgecolor='blue', linewidth=0.0003, antialiased=True)
-        ax.set_xlabel('x')
-        ax.set_ylabel('y')
-        ax.set_zlabel('u')
-        path = "../pictures/exact_solution/%i.png" % k
-        plt.savefig(path)
-        plt.close(fig)
-        k = k + 1
+    # plot the approximated values
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.set_zlim([-1, 1])
+    ax.plot_surface(_X, _Y, Z_surface, cmap=cm.RdYlBu_r, edgecolor='blue', linewidth=0.0003, antialiased=True)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('u')
+    path = "../pictures/approxmate_solution/%i.png" % k
+    plt.savefig(path)
+    plt.close(fig)
+    # plot the exact solution
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.set_zlim([-1, 1])
+    ax.plot_surface(_X, _Y, True_Z_surface, cmap=cm.RdYlBu_r, edgecolor='blue', linewidth=0.0003, antialiased=True)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('u')
+    path = "../pictures/exact_solution/%i.png" % k
+    plt.savefig(path)
+    plt.close(fig)
+    k = k + 1
 
